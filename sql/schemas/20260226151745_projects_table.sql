0fad390eba603974cf21ddda9feac1df766c5012
Purinutt Amartayavis <ice.ice5335@gmail.com>
2026-02-26 22:29:50 +0700

    Add: Project table

---
+++ sql/schemas/20260226151745_projects_table.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+CREATE TABLE projects (
+	id		UUID	UNIQUE NOT NULL DEFAULT gen_random_uuid(),
+	name	TEXT	NOT NULL,
+	picture	TEXT,
+
+	PRIMARY KEY(id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+DROP TABLE projects;
+-- +goose StatementEnd
